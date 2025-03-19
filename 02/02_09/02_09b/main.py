6d68435427393388c89dcc4b700aec4f6324afd7
Sajee <19692693+speeder990@users.noreply.github.com>
2025-03-19 15:08:23 +0000

    update files upto chapter 3

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,7 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 2:
+        return None
+    else:
+        return(sorted(my_list)[1])
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -8,7 +8,12 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    return {}
+    duplicate_user_pref = user_pref
+    for key, value in user_pref.items():
+        if value == None:
+            del duplicate_user_pref[key] # this doesn't work
+    return duplicate_user_pref
 
 
 print(update_preferences(user_preferences))
+print(user_preferences)
